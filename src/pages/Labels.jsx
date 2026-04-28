043a7b492cd7062bfb139976c04a70fe092bdd6b
umesh-vetchat <umesh@vetchat.com.au>
2026-04-29 09:37:23 +1000

    fix: split address lines to ensure proper formatting for multi-line shipping labels

--- src/pages/Labels.jsx
+++ src/pages/Labels.jsx
@@ -94,7 +94,8 @@ export default function Labels() {
                 <span className="label-to">To</span>
                 
                 <strong className="label-name">{order.name} <span className="label-orderID">({order.orderId})</span></strong>
-                <span className="label-address">{order.address1}{order.address2 ? `, ${order.address2}` : ''}</span>
+                <span className="label-address">{order.address1},</span>
+                <span className="label-address">{order.address2 ? `${order.address2}` : ''}</span>
                 <span className="label-address">{order.city} {order.state} {order.postcode}</span>
                 {order.country && order.country.toLowerCase() !== 'australia' && (
                   <span className="label-address">{order.country}</span>
